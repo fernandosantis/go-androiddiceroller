a40e02ec498df3481de34de059abbef895b85bd5
Fernando Santis <fpsantis@gmail.com>
2021-07-13 06:09:14 -0300

    Exercise Complete

--- app/src/main/java/curso/google/androiddiceroller/MainActivity.kt
+++ app/src/main/java/curso/google/androiddiceroller/MainActivity.kt
@@ -2,6 +2,7 @@ package curso.google.androiddiceroller
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,24 +13,50 @@ import androidx.appcompat.app.AppCompatActivity
  */
 
 class MainActivity : AppCompatActivity() {
+    var sorta = Dice(6).roll()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollButton: Button = findViewById(R.id.bt_roll)
         rollButton.setOnClickListener { rollDice() }
+        val resulte: TextView = findViewById(R.id.tv_result)
+        resulte.text = "Lucky Num: $sorta"
     }
 
-    /**
+    /*
      * Roll the dice and update the screen with the result.
      */
     private fun rollDice() {
         // Create new Dice object with 6 sides and roll it
+        val diceImage: ImageView = findViewById(R.id.iv_dice)
+        diceImage.setImageResource(R.drawable.dice_2)
+
         val dice = Dice(6).roll()
+        diceImage.setImageResource(
+            when (dice) {
+                1 -> R.drawable.dice_1
+                2 -> R.drawable.dice_2
+                3 -> R.drawable.dice_3
+                4 -> R.drawable.dice_4
+                5 -> R.drawable.dice_5
+                else -> R.drawable.dice_6
+            }
+        )
+
+        var msg = ""
+        if (dice == sorta) {
+            msg = "Congrats! You Win"
+        } else {
+            msg = "Oh, no... Try Again"
+        }
         // Update the screen with the dice roll
-        val result: TextView = findViewById(R.id.tv_result)
-        result.text = dice.toString()
-        Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT).show()
+        Toast.makeText(this, "Dice Rolled! $msg", Toast.LENGTH_SHORT).show()
+        sorta = Dice(6).roll()
+        val resulte: TextView = findViewById(R.id.tv_result)
+        resulte.text = "Lucky Num: $sorta"
     }
 }
 
